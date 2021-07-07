4d1ea36502409af96dddeeba1eacdb88bc11350e
magnetoneSama <magnetone.65@gmail.com>
2021-07-07 23:10:29 +0200

    initial commit

---
+++ tournament.py
@@ -0,0 +1,23 @@
+from datetime import datetime
+from time import strftime
+
+
+class Tournament:
+    description = " ceci est un tournoi d'échec"
+
+    def __init__(self, name, place="local", turn_played=4, players_numbers=8, time_control="blitz"):
+        """init values of this tournament"""
+        self.players = []
+        self.name = name
+        self.place = place
+        self.turn_played = turn_played
+        self.players_numbers = players_numbers
+        self.time_control = time_control
+
+    def add_player(self, player):
+        """add an player in the players list of this tournament"""
+        self.players.append(player)
+
+    def del_player(self,player):
+        """del an player in the players list of this tournament"""
+        self.players.remove(player)
